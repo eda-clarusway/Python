8e13ded1498d9b435db6574539741e701ad344aa
eda <101637060+eda-clarusway@users.noreply.github.com>
2022-05-02 15:48:25 +0300

    added

---
+++ mini_calculator.py
@@ -0,0 +1,15 @@
+def calculator(x, y, opr) :
+    if opr == "+" :
+        print(x + y)
+        
+    elif opr == "-" :
+        print(x - y)
+        
+    elif opr == "*" :
+        print(x * y)
+        
+    elif opr == "/" :
+        print(x / y)
+        
+    else :
+        print("Enter a valid operator.")
