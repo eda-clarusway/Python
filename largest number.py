e63885005a65a69312069064eb3455cbde8c1e77
eda <101637060+eda-clarusway@users.noreply.github.com>
2022-05-29 23:10:50 +0300

    added

---
+++ largest number.py
@@ -0,0 +1,13 @@
+x = int(input("please an integer number: "))
+y = int(input("please an integer number again: "))
+z = int(input("please an integer number again: "))
+t = int(input("please an integer number again: "))
+w = int(input("please an integer number again: "))
+my_list = []
+my_list.append(x)
+my_list.append(y)
+my_list.append(z)
+my_list.append(t)
+my_list.append(w)
+my_list.sort()
+print(my_list[-1])
