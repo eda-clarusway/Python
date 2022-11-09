7f118df35a18926b3122ad09765dda3f6de79580
“eda” <acikgozeda8588@gmail.com>
2022-11-09 12:49:01 +0300

    added

---
+++ ages.py
@@ -0,0 +1,9 @@
+def yasHesapla(dogumYili):
+    return 2019 - dogumYili
+
+ageCinar = yasHesapla(2017)
+ageAda = yasHesapla(2010)
+ageSena = yasHesapla(1999)
+
+print(ageCinar, ageAda, ageSena)
+# age calculate.....
